4c7b147d7ffa88670734dce20cc2c1a426b33244
WeeBHaCKeR <118058592+weebhacker@users.noreply.github.com>
2022-12-05 23:13:32 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import time 
+import os
+from random import randint
+import webbrowser 
+os.system("color a")
+print("------------------LOG IN-------------------------")
+login = input("Login \n >..  ")
+if login == 'WeeBHaCKeR1':
+	print()
+	os.system("color a")
+	password = input("Password \n >..  ")
+	if password == '192//268':
+		print("-------------------------------------------------")
+		time.sleep(2)
+		os.system("color a")
+		print("[+]Successfull")
+		time.sleep(2)	
+		os.system('pip install requests')	
+		print("[+]Requests")
+		time.sleep(2)
+		os.system("color a")
+		os.system('pip install requirements')
+		print("[+]Requirements")
+		time.sleep(2)
+		os.system("color a")
+		print("[+]Hacking tools")
+		time.sleep(2)
+		os.system("color a")
+		os.system('pip install python')
+		print("[+]Install python")
+		time.sleep(2)
+		os.system("color a")
+		print("-------------------------------------------------")
+		time.sleep(1)
+	else:
+		print("------------------password error-----------------")
+			  
+
+else:
+	print("------------------login error---------------")
